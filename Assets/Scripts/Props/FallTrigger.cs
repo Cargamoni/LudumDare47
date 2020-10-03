d0da17fb9d2dcbfd42f495f0a88d0ecba4eacd97
Mustafa Çetin <mstplus@gmail.com>
2020-10-03 16:49:08 -0300

    .

---
+++ Assets/Scripts/Props/FallTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FallTrigger : MonoBehaviour {
+
+    public Rigidbody target;
+    public AudioClip fallSound;
+
+    private void OnTriggerEnter(Collider collision) {
+        if(collision.gameObject != target.gameObject) {
+            target.isKinematic = false;
+            AudioManager.instance.playSfx(fallSound);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Props/Switch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Switch : Action {
+
+    public bool on;
+    public GameObject[] targets;
+    public AudioClip sound;
+
+    public override AudioClip getSound() {
+        return sound;
+    }
+
+    public override string getText() {
+        return on ? "Turn Off" : "Turn On";
+    }
+
+    public override void invoke() {
+        on = !on;
+        if (targets != null) {
+            foreach (var i in targets) {
+                i.SetActive(!i.activeSelf);
+            }
+        }
+    }
+
+}
