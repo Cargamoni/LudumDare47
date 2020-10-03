4635662e3a57baa50ee538eff10a24b2c70fe5a2
Mustafa Çetin <mstplus@gmail.com>
2020-10-03 10:11:20 -0300

    sahne denemeleri

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+
+    public AudioClip bounceSound;
+
+    [Range(0f, 1f)]
+    public float scale = 0.5f;
+
+    private void OnCollisionEnter(Collision collision) {
+        var value = Mathf.Clamp01((collision.relativeVelocity.magnitude * 10f) * scale);
+        AudioManager.instance.playSfx(bounceSound, transform.position, value);
+    }
+
+}
