3c9fb7d01781f8c57506b7ad7e4cb5d43c97597e
riwmd <kubilayege95@gmail.com>
2020-10-03 14:58:21 +0300

    patlar bu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Screens
+{
+    Main,
+    Settings,
+    InGame,
+    About,
+    Win,
+    Lose
+}
+
+public class UIManager : MonoBehaviour
+{
+    private int currentScreenIndex;
+    public GameObject[] screens;
+
+    public void ChangeScreen(int screen)
+    {
+        screens[currentScreenIndex].SetActive(false);
+        screens[screen].SetActive(true);
+        currentScreenIndex = screen;
+    }
+}
