dd3a3f9146638472dd2ec2205dc44103b716523c
riwmd <kubilayege95@gmail.com>
2020-10-03 18:02:07 +0300

    uiWIP

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public enum Screens {
     Main,
@@ -14,16 +15,50 @@ public class UIManager : MonoBehaviour {
     private int currentScreenIndex;
     public GameObject[] screens;
     public static UIManager instance;
+    public Slider masterVolume;
+    public Slider musicVolume;
+    public Slider sfxVolume;
+
 
     private void Awake()
     {
         instance = this;
+        
+    }
+
+    public void Start()
+    {
+        masterVolume.onValueChanged.AddListener(OnMasterVolumeChanged);
+        musicVolume.onValueChanged.AddListener(OnMusicVolumeChanged);
+        sfxVolume.onValueChanged.AddListener(OnSfxVolumeChanged);
+
+        masterVolume.value = AudioManager.instance.getMasterVolume();
+        musicVolume.value = AudioManager.instance.getMusicVolume();
+        sfxVolume.value = AudioManager.instance.getSfxVolume();
     }
 
     public void ChangeScreen(int screen) {
         screens[currentScreenIndex].SetActive(false);
         screens[screen].SetActive(true);
         currentScreenIndex = screen;
+    }
+
+    public void OnMasterVolumeChanged(float value)
+    {
+        AudioManager.instance.setMasterVolume(value);
+        Debug.Log(value);
+    }
+
+    public void OnMusicVolumeChanged(float value)
+    {
+        AudioManager.instance.setMusicVolume(value);
+        Debug.Log(value);
+    }
+
+    public void OnSfxVolumeChanged(float value)
+    {
+        AudioManager.instance.setSfxVolume(value);
+        Debug.Log(value);
     }
 
 }
