84d5dc079a7248bc23905724cf6c957f109ed3c5
Mustafa Çetin <mstplus@gmail.com>
2020-10-04 17:34:15 -0300

    commit şart

---
+++ Assets/Scripts/Props/Force.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Force : MonoBehaviour{
+
+    public float triggerRadius = 2f;
+    public GameObject effect;
+    public LayerMask layerMask;
+
+    private void Update() {
+        if (Physics.CheckSphere(transform.position, triggerRadius, layerMask.value)) {
+            if (effect != null) Instantiate(effect, transform.position, Quaternion.identity);
+            var rb = GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(Random.Range(-100f, +100f), 500f, Random.Range(-100f, +100f)));
+            Destroy(this);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Props/WallFall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WallFall : MonoBehaviour{
+
+    public GameObject explotionPrefab;
+    public float triggerRadius = 2f;
+
+    public GameObject effect;
+    public Rigidbody rb;
+    public LayerMask rayMask;
+
+    void Update() {
+        if (Physics.CheckSphere(transform.position, triggerRadius, rayMask.value)) {
+
+            if(effect != null) effect.SetActive(true);
+            rb.isKinematic = false;
+            Instantiate(explotionPrefab, transform.position, Quaternion.identity);
+
+            Player.instance.die();
+            rb.AddExplosionForce(10f, Random.insideUnitSphere, 0.5f);
+
+            Destroy(this);
+
+        }
+    }
+
+}
