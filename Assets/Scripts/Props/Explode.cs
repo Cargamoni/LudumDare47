e296321a817863b6e0df62697cce84770afbea4c
Mustafa Çetin <mstplus@gmail.com>
2020-10-04 18:15:05 -0300

    .

---
+++ Assets/Scripts/Props/Explode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+    public GameObject explotionPrefab;
+    public float triggerRadius = 2f;
+
+    public GameObject effect;
+    public Rigidbody rb;
+    public LayerMask rayMask;
+
+    void Update() {
+        if (Vector3.Distance(transform.position, Player.instance.transform.position) < triggerRadius) {
+            if (Physics.Linecast(transform.position, Player.instance.transform.position, rayMask)) {
+
+                effect.SetActive(true);
+                rb.isKinematic = false;
+                Instantiate(explotionPrefab, transform.position, Quaternion.identity);
+
+                Player.instance.die();
+                rb.AddExplosionForce(10f, Random.insideUnitSphere, 0.5f);
+
+                Destroy(this);
+
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Props/Force.cs
+++ Assets/Scripts/Props/Force.cs
@@ -4,16 +4,27 @@
 public class Force : MonoBehaviour{
 
     public float triggerRadius = 2f;
+    public bool explotion;
     public GameObject effect;
     public LayerMask layerMask;
 
     private void Update() {
         if (Physics.CheckSphere(transform.position, triggerRadius, layerMask.value)) {
-            if (effect != null) Instantiate(effect, transform.position, Quaternion.identity);
+
             var rb = GetComponent<Rigidbody>();
             rb.isKinematic = false;
             rb.AddForce(new Vector3(Random.Range(-100f, +100f), 300f, Random.Range(-100f, +100f)));
+
+            if (effect != null) {
+                Instantiate(effect, transform.position, Quaternion.identity);
+            }
+
+            if (explotion) {
+                Player.instance.die();
+            }
+
             Destroy(this);
+
         }
     }
 
