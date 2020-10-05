7f6f2b7d585d7ce1afe33425fbc8f3cc411e545e
riwmd <kubilayege95@gmail.com>
2020-10-05 20:39:30 +0300

    finalsceneFade

--- Assets/Scripts/Props/Exit.cs
+++ Assets/Scripts/Props/Exit.cs
@@ -5,6 +5,7 @@ public class Exit : MonoBehaviour {
 
     private void Update() {
         if (Vector3.Distance(transform.position, Player.instance.transform.position) < 3f) {
+            GameManager.currentState = GameStates.Playing;
             SceneManager.LoadScene("Final");
         }
     }
