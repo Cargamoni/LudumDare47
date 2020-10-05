683d509c64e07a53a0b03c2324c4458e83067cb8
Mustafa Çetin <mstplus@gmail.com>
2020-10-05 16:00:45 -0300

    car crash

--- Assets/Scripts/Core/Out/Player2.cs
+++ Assets/Scripts/Core/Out/Player2.cs
@@ -7,6 +7,7 @@ public class Player2 : MonoBehaviour{
     public Fade fade;
     public static Player2 instance;
     public bool isDead;
+    public AudioClip dieSound;
 
     void Start() {
         instance = this;
@@ -15,13 +16,13 @@ public class Player2 : MonoBehaviour{
     public void die() {
         if (isDead) return;
         isDead = true;
-
+        AudioManager.instance.playSfx(dieSound);
         StartCoroutine(dieCoroutine());
     }
 
     private IEnumerator dieCoroutine() {
         fade.gameObject.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1f);
         Time.timeScale = 1f;
 
         GameManager.currentState = GameStates.Playing;
