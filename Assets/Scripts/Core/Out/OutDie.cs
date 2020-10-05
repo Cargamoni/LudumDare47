b38b542c71d8b208626b8522c77840cad0cc1607
Mustafa Çetin <mstplus@gmail.com>
2020-10-04 23:17:20 -0300

    Revert "Revert ".""
    This reverts commit 30b60080550bddb002512d77795256f5520d0831.

---
+++ Assets/Scripts/Core/Out/OutDie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutDie : MonoBehaviour {
+
+    public Fade fade;
+
+    public OutDie instance;
+
+    void Start() {
+        instance = this;
+    }
+
+    
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
