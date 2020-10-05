80abb2a9e500f826d8fb030c41f00e499f7d63c1
Mustafa Çetin <mstplus@gmail.com>
2020-10-04 23:15:10 -0300

    Revert "Revert ".""
    This reverts commit 30b60080550bddb002512d77795256f5520d0831.

---
+++ Assets/Scripts/Core/Out/Player2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player2 : MonoBehaviour{
+
+    public Fade fade;
+    public static Player2 instance;
+    public bool isDead;
+
+    void Start() {
+        instance = this;
+    }
+
+    public void die() {
+        if (isDead) return;
+        isDead = true;
+        StartCoroutine(dieCoroutine());
+    }
+
+    private IEnumerator dieCoroutine() {
+        fade.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("Mustafa");
+    }
+
+}
